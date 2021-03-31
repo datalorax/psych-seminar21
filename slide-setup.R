30c6d5bf7ea3a8b64ab183c02a077f09a5e0083e
Daniel Anderson <daniela@uoregon.edu>
2021-03-31 15:47:47 -0700

    Add pdf & fix footer links

--- slide-setup.R
+++ slide-setup.R
@@ -13,7 +13,7 @@ setup <- function(slides) {
     <i class = "footer-icon" data-feather="download"></i>
   </a>
   
-  <a class = "footer-icon-link" href = "https://datalorax.github.io/psych-seminar21.html">
+  <a class = "footer-icon-link" href = "https://datalorax.github.io/psych-seminar21/">
     <i class = "footer-icon" data-feather="link"></i>
   </a>
   
